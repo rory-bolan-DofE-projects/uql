27ee8fb23c01e3a0360f5dec6564d0a6e730c95c
= <=>
2026-08-28 17:29:59 +0200

    added an api access point using Spark Java, connect to it using xxx.xxx.xxx.xxx:8080 and just use a post request with your query looking like this: {  "requests": ["request", "request", "request", "request"...] } this was done using the google gson library, as well as a couple of helper functions, like Parser.Response.responsesToJson(Parser.Response[]), which uses gson to turn the typical array of responses that you get from Parser.execute() into valid json

--- src/main/java/app/belgarion/java/db_files/Main.java
+++ src/main/java/app/belgarion/java/db_files/Main.java
@@ -1,100 +1,72 @@
 package app.belgarion.java.db_files;
 
-
-
 import app.belgarion.java.uql.Parser;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.Buffer;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Objects;
 
-//import java.io.IOException;
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//import static app.belgarion.java.db_files.Database.*;
-
-
+import static spark.Spark.*;
 
 public class Main {
     public static void run(String dbName, String[] args) throws IOException, Database.MalformedRequestException, Parser.IncorrectQuerySyntaxException, Parser.MalformedTableException {
+        System.out.println("--- 1. Creating Database ---");
+        Database.New(dbName);
 
+        System.out.println("\n2. Executing UQL Queries");
+        Parser.Response[] responses = Parser.execute(args);
 
+        System.out.println("\n3. Query Results");
+        for (int i = 0; i < responses.length; i++) {
+            System.out.println("Query " + (i + 1) + ":");
+            System.out.println(responses[i].toString());
+        }
 
-                    System.out.println("--- 1. Creating Database ---");
-                    Database.New(dbName);
-
-
-
-
-                    System.out.println("\n2. Executing UQL Queries");
-                    Parser.Response[] responses = Parser.execute(args);
-
-
-                    System.out.println("\n3. Query Results");
-                    for (int i = 0; i < responses.length; i++) {
-                        System.out.println("Query " + (i + 1) + ":");
-                        System.out.println(responses[i].toString());
-                    }
-
-                    System.out.println("4. Internal Database Dump");
-                    Database.getAll(dbName);
-
-
-
-
-//        if (Arrays.equals(args, new String[]{})) {
-//            CLI();
-//            System.exit(1);
-//        }
-//        String command = args[0];
-//        String file = args[1];
-//        if (!command.equals("new") && !command.equals("load") && !command.equals("create-table")) {
-//            System.err.println("Unknown command: " + command);
-//            System.exit(1);
-//        }
-//        if (command.equals("new")) {
-//            if (file == null) {
-//                System.err.println("Missing command line argument: database file");
-//                System.exit(1);
-//            }
-//            if (!file.endsWith(".udb")) {
-//                System.err.printf("File '%s' does not end with .udb\n", file);
-//                System.exit(1);
-//            }
-//            New(file);
-//        } else if (command.equals("load")) {
-//            if (file == null) {
-//                System.err.println("Missing command line argument: database file");
-//                System.exit(1);
-//            }
-//            if (!file.endsWith(".udb")) {
-//                System.err.printf("File '%s' does not end with .udb%n", file);
-//                System.exit(1);
-//            }
-//            Database.getAll(file);
-//
-//        } else {
-//            // uql create-table filename table_name columns
-//            List<String> arguments = new ArrayList<>(List.of(args));
-//            arguments.removeFirst();
-//            Database.createTable(arguments);
-//        }
-
+        System.out.println("4. Internal Database Dump");
+        Database.getAll(dbName);
     }
 
+    private static void api() {
+        Gson gson = new Gson();
+        port(8080);
+        post("/", (req, res) -> {
+            res.type("application/json");
+            try {
+                JSONbody body = gson.fromJson(req.body(), JSONbody.class);
+                if (body == null || body.getRequests() == null || body.getRequests().length == 0) {
+                    res.status(400);
+                    return "{\"error\": \"Missing or empty field 'requests' in JSON body\"}";
+                }
+                Parser.Response[] resp = Parser.execute(body.getRequests());
 
+                return Parser.Response.responsesToJson(resp);
+            } catch (Exception e) {
+                res.status(500);
+                JsonObject err = new JsonObject();
+                err.addProperty("error", e.getMessage());
+                return gson.toJson(err);
+            }
+        });
+    }
 
+    private static class JSONbody {
+        private String[] requests;
+        public String[] getRequests() { return requests; }
+    }
 
     public static void main(String[] args) throws Database.MalformedRequestException, Parser.IncorrectQuerySyntaxException, Parser.MalformedTableException, IOException {
         if (args.length == 0) return;
+        if (Objects.equals(args[0], "api")) {
+            api();
+            return;
+        }
         if (!Files.exists(Path.of(args[0]))) return;
         ArrayList<String> list = new ArrayList<>();
         BufferedReader reader;
